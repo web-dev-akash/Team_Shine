4e6f232bf76be41d2916e9d708758e7920782bf0
web-dev-akash <akumar415129.ak@gmail.com>
2022-08-04 01:13:32 +0530

    Index and Cart Page

---
+++ index.js
@@ -0,0 +1,278 @@
+var courseData = [
+  {
+    name: "Backend Developer in Java",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1640851375_8085.png&w=1920&q=75",
+  },
+  {
+    name: "Backend Developer in Python",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1640851637_4621.png&w=1920&q=75",
+  },
+  {
+    name: "Backend Developer in Node.js",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1652953799_9984.png&w=1920&q=75",
+  },
+  {
+    name: "Backend Developer in .Net",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1652953980_7255.png&w=1920&q=75",
+  },
+  {
+    name: "Frontend Developer in React.js",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1652954044_279.png&w=1920&q=75",
+  },
+  {
+    name: "Frontend Developer in Angular.js",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1640853726_1126.png&w=1920&q=75",
+  },
+  {
+    name: "Android Developer",
+    price: 12999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1652954087_2845.png&w=1920&q=75",
+  },
+  {
+    name: "CompTIA Security+ Certification Training - SY0-601",
+    price: 34999,
+    category: "IT",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fattachment%2Fdefault_product_image.jpg&w=1920&q=75",
+  },
+  {
+    name: "Digital Marketing Certification Training",
+    price: 19995,
+    category: "Marketing",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1542800087_8980.png&w=1920&q=75",
+  },
+  {
+    name: "Marketing Essentials",
+    price: 9620,
+    category: "Marketing",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1508900890_4134.png&w=1920&q=75",
+  },
+  {
+    name: "Mastering Google Analytics",
+    price: 7999,
+    category: "Marketing",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1652954139_2930.png&w=1920&q=75",
+  },
+  {
+    name: "Human Resources Management Training",
+    price: 12999,
+    category: "HR",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1564143982_3906.png&w=1920&q=75",
+  },
+  {
+    name: "Certified HR Analytics Professional",
+    price: 3499,
+    category: "HR",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1634126540_4125.png&w=1920&q=75",
+  },
+  {
+    name: "Management Bootcamp",
+    price: 8000,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1640689215_9063.png&w=1920&q=75",
+  },
+  {
+    name: "Certified Manager for Quality and Organisational",
+    price: 11360,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1508160736_4805.png&w=1920&q=75",
+  },
+  {
+    name: "Master of Project Management",
+    price: 19999,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1617699867_4091.png&w=1920&q=75",
+  },
+  {
+    name: "Management Fundamentals Training Course",
+    price: 16499,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1579153944_2178.png&w=1920&q=75",
+  },
+  {
+    name: "Management Fundamentals Training & Certification",
+    price: 19999,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1588266994_3384.png&w=1920&q=75",
+  },
+  {
+    name: "Advanced MS Excel 2016 Certification Training",
+    price: 8475,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fattachment%2Fdefault_product_image.jpg&w=1920&q=75",
+  },
+  {
+    name: "Business Communication Specialist",
+    price: 3499,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fproduct_image%2Fimages_all%2F1508153062_3057.png&w=1920&q=75",
+  },
+  {
+    name: "Certiication in Concrete Testing",
+    price: 200,
+    category: "Management",
+    image:
+      "https://learning.shine.com/_next/image?url=https%3A%2F%2Fstatic1.shine.com%2Fl%2Fm%2Fattachment%2Fdefault_product_image.jpg&w=1920&q=75",
+  },
+];
+
+// Slider function start-----------------------
+var count = 1;
+setInterval(function () {
+  document.getElementById("radio" + count).checked = true;
+  count++;
+  if (count > 6) {
+    count = 1;
+  }
+}, 3000);
+// Slider function End ------------------------
+
+// Sticky Navbar start------------------
+window.onscroll = function () {
+  stickyNavbar();
+};
+
+var navbar = document.getElementById("navbar-container");
+var sticky_navbar = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky_navbar) {
+    navbar.classList.add("sticky_navbar");
+  } else {
+    navbar.classList.remove("sticky_navbar");
+  }
+}
+
+// Sticky Navbar End---------------------
+
+// Adding to cart and storing it to localStorage-----------
+// var cartArr = [];
+
+// var cartData = JSON.parse(localStorage.getItem("cart-items")) || [];
+
+function emptyCart() {
+  var div = document.createElement("div");
+  div.setAttribute("class", "empty-cart-div");
+  var image = document.createElement("img");
+  image.setAttribute(
+    "src",
+    "https://learning.shine.com/_next/image?url=%2Fimages%2Fno-item-cart.png&w=64&q=75"
+  );
+  var h4 = document.createElement("h4");
+  h4.textContent = "No Items in Cart";
+  var para = document.createElement("p");
+  para.textContent = "Keep exploring to find a course for you";
+  var exploreBtn = document.createElement("a");
+  exploreBtn.innerText = "Explore Now";
+  exploreBtn.setAttribute("class", "cart-explore-now");
+  div.append(image, h4, para, exploreBtn);
+  document.getElementById("cart-data").append(div);
+
+  document.getElementById("cart-total").style.display = "none";
+}
+
+var cartPageData = JSON.parse(localStorage.getItem("cart-items")) || [];
+function displayProducts() {
+  var totalCost = 0;
+  cartPageData.map(function (elem, index) {
+    // cart items div---------------------------
+
+    var divMain = document.createElement("div");
+    divMain.setAttribute("id", "cart-items-container");
+
+    var divLeft = document.createElement("div");
+    divLeft.setAttribute("id", "use-flex-box-here");
+    // var divImage = document.createElement("div");
+    var image = document.createElement("img");
+    image.setAttribute("src", elem.image);
+    image.setAttribute("alt", index);
+
+    var divName = document.createElement("div");
+    var h3 = document.createElement("h3");
+    h3.innerText = elem.name;
+    var price = document.createElement("h4");
+    price.setAttribute("id", "course-price");
+    price.innerText = "₹" + elem.price;
+
+    var deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("id", "remove-btn");
+    deleteBtn.innerHTML =
+      '<span class="material-symbols-outlined">delete</span>';
+
+    divName.append(h3, price);
+    divLeft.append(image, divName);
+
+    divMain.append(divLeft, deleteBtn);
+    document.getElementById("cart-data").append(divMain);
+
+    totalCost += elem.price;
+    document.getElementById("ak-cart-subtotal").innerText = "₹ " + totalCost;
+    var sgstPrice = (totalCost * 9) / 100;
+    document.getElementById("ak-sgst-price").innerText = "+ ₹ " + sgstPrice;
+    var cgstPrice = (totalCost * 9) / 100;
+    document.getElementById("ak-cgst-price").innerText = "+ ₹ " + cgstPrice;
+    var totalPayble = totalCost + sgstPrice + cgstPrice;
+    document.getElementById("ak-total-price-cart").innerText =
+      "₹ " + totalPayble.toFixed(2);
+  });
+}
+
+var cart = document.querySelectorAll("#corseexpert");
+
+for (let i = 0; i < cart.length; i++) {
+  cart[i].addEventListener("click", function () {
+    addToCart(i);
+  });
+}
+
+var cartArr = JSON.parse(localStorage.getItem("cart-items")) || [];
+function addToCart(index) {
+  var course = courseData.filter(function (elem, i) {
+    return i == index;
+  });
+  // if (!cartArr.includes(course[0].name)) {
+  //   console.log();
+  cartArr.push(course[0]);
+  //   }
+
+  localStorage.setItem("cart-items", JSON.stringify(cartArr));
+}
+if (cartArr.length === 0) {
+  emptyCart();
+} else {
+  displayProducts();
+}
+
+window.onload = console.log(cartArr[0].name);
